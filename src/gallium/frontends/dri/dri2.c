a96d33f8d4c0f95c60fe2179d4173e9c6944620f
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-08-05 10:19:34 +0800

    Update dri2.c

--- src/gallium/frontends/dri/dri2.c
+++ src/gallium/frontends/dri/dri2.c
@@ -1419,12 +1419,6 @@ dri2_from_dma_bufs(struct dri_screen *screen,
    struct dri_image *img;
    const struct dri2_format_mapping *map = dri2_get_mapping_by_fourcc(fourcc);
 
-   if (!screen->dmabuf_import) {
-      if (error)
-         *error = __DRI_IMAGE_ERROR_BAD_PARAMETER;
-      return NULL;
-   }
-
    unsigned err = __DRI_IMAGE_ERROR_SUCCESS;
    /* Allow a NULL error arg since many callers don't care. */
    unsigned unused_error;
